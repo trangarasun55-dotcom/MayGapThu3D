1c50d4e5937af6e067321d9ae4464ea9add24def
Hong Nhung <luonghongnhung.12a7.tnd@gmail.com>
2026-05-30 20:52:57 +0700

    x

--- render.cpp
+++ render.cpp
@@ -6,7 +6,7 @@
 #include "imageloader.h"
 
 // BIẾN LƯU ID CỦA CÁC TEXTURE ĐƯỢC SỬ DỤNG TRONG CẢNH VẬT
-GLuint texture_nensao, texture_saosang, texture_saoroi, texture_suoisao;
+GLuint texture_saosang, texture_osao, texture_saoroi;
 GLuint texture_kim;
 GLuint texture_longxam, texture_longxanhduong, texture_longxanhla, texture_longhong, texture_longkem;
 GLuint texture_may, texture_maysao;
@@ -183,10 +183,10 @@ void drawControlPanel() {
 // HÀM VẼ THÂN MÁY GẮP, BAO GỒM THÂN VỎ CHÍNH, MÁI CHE, CỘT TRỤ ĐỨNG 4 GÓC, BÀN ĐIỀU KHIỂN VÀ HỆ THỐNG CỬA SẬP XẢ QUÀ CHÍNH GIỮA MẶT TRƯỚC THÂN MÁY
 void drawMachineBody() {
     // Thân vỏ bên dưới máy
-    drawRoundedBox(texture_nensao, 0.0f, -2.1f, 0.0f, 3.5f, 3.2f, 3.5f);
+    drawRoundedBox(texture_osao, 0.0f, -2.1f, 0.0f, 3.5f, 3.2f, 3.5f);
 
     // Mái máy che bên trên lồng
-    drawRoundedBox(texture_nensao, 0.0f, 4.0f, 0.0f, 3.5f, 0.8f, 3.5f);
+    drawRoundedBox(texture_osao, 0.0f, 4.0f, 0.0f, 3.5f, 0.8f, 3.5f);
 
 	// Cột trụ đứng 4 góc xung quanh thân máy
     float pO = 1.6f;
@@ -218,7 +218,7 @@ void drawMachineBody() {
 
     // 3. Tịnh tiến ngược lên một nửa chiều cao tấm cửa để định tâm vẽ khối hộp chuẩn
     // Vì cửa cao 1.4f, tâm hình học của nó sẽ cách bản lề đáy một khoảng Y = +0.7f
-    drawRoundedBox(texture_maysao, 0.0f, 0.7f, 0.0f, 1.4f, 1.4f, 0.05f);
+    drawRoundedBox(texture_saosang, 0.0f, 0.7f, 0.0f, 1.4f, 1.4f, 0.05f);
 
     // Vẽ tay nắm cửa xả ở phía trên đầu tấm cửa (cách đáy bản lề Y = 1.2f)
     drawRoundedBox(texture_kim, 0.0f, 1.2f, 0.05f, 0.6f, 0.1f, 0.08f);
@@ -649,9 +649,9 @@ void initGraphics() {
 
     texture_kim = loadTexture("data/kim.bmp");
 
-    texture_suoisao = loadTexture("data/suoisao.bmp");
+    texture_osao = loadTexture("data/osao.bmp");
     texture_saoroi = loadTexture("data/saoroi.bmp");
-    texture_nensao = loadTexture("data/nensao.bmp");
+    texture_saosang = loadTexture("data/saosang.bmp");
 
     texture_may = loadTexture("data/may.bmp");
     texture_maysao = loadTexture("data/maysao.bmp");
